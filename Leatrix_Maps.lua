d97a26ac7e9a53a1f27c7e916352934f740302a8
leatrix <58888415+leatrix@users.noreply.github.com>
2021-12-08 13:32:00 +0000

    2.5.73

--- Leatrix_Maps.lua
+++ Leatrix_Maps.lua
@@ -1,6 +1,6 @@
 ﻿
 	----------------------------------------------------------------------
-	-- 	Leatrix Maps 2.5.71.alpha.7 (4th December 2021)
+	-- 	Leatrix Maps 2.5.73 (8th December 2021)
 	----------------------------------------------------------------------
 
 	-- 10:Func, 20:Comm, 30:Evnt, 40:Panl
@@ -12,7 +12,7 @@
 	local LeaMapsLC, LeaMapsCB, LeaDropList, LeaConfigList = {}, {}, {}, {}
 
 	-- Version
-	LeaMapsLC["AddonVer"] = "2.5.71.alpha.7"
+	LeaMapsLC["AddonVer"] = "2.5.73"
 
 	-- Get locale table
 	local void, Leatrix_Maps = ...
