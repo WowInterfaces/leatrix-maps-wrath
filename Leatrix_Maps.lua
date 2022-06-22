e1480c0002510fe75d3e11cfb337ea73e3b6ccc4
leatrix <58888415+leatrix@users.noreply.github.com>
2022-06-22 09:24:33 +0100

    2.5.111

--- Leatrix_Maps.lua
+++ Leatrix_Maps.lua
@@ -1,6 +1,6 @@
 ﻿
 	----------------------------------------------------------------------
-	-- 	Leatrix Maps 2.5.110 (15th June 2022)
+	-- 	Leatrix Maps 2.5.111 (22nd June 2022)
 	----------------------------------------------------------------------
 
 	-- 10:Func, 20:Comm, 30:Evnt, 40:Panl
@@ -12,7 +12,7 @@
 	local LeaMapsLC, LeaMapsCB, LeaDropList, LeaConfigList = {}, {}, {}, {}
 
 	-- Version
-	LeaMapsLC["AddonVer"] = "2.5.110"
+	LeaMapsLC["AddonVer"] = "2.5.111"
 
 	-- Get locale table
 	local void, Leatrix_Maps = ...
@@ -29,6 +29,9 @@
 			return
 		end
 	end
+
+	-- Set bindings translations
+	_G.BINDING_NAME_LEATRIX_MAPS_GLOBAL_TOGGLE = L["Toggle panel"]
 
 	----------------------------------------------------------------------
 	-- L00: Leatrix Maps
