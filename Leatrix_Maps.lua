18963a3f9f3f11cac573e8126c73de1141785883
leatrix <58888415+leatrix@users.noreply.github.com>
2022-07-15 10:41:54 +0100

    2.5.115.alpha.1

--- Leatrix_Maps.lua
+++ Leatrix_Maps.lua
@@ -85,6 +85,7 @@
 			local nodd = LeaMapsLC:CreateDropDown("ZoneMapNoneMenu", "", WorldMapFrame, 180, "TOP", -80, -35, {"---"}, "")
 			nodd:ClearAllPoints()
 			nodd:SetPoint("TOPRIGHT", outerFrame, "TOPRIGHT", 0, 0)
+			nodd.btn:Disable()
 
 			-- Create Eastern Kingdoms dropdown menu
 			LeaMapsLC["ZoneMapEasternMenu"] = 1
@@ -3529,6 +3530,7 @@
 			dbtn.tiptext = tip; dbtn:SetScript("OnEnter", LeaMapsLC.ShowTooltip)
 			dbtn:SetScript("OnLeave", GameTooltip_Hide)
 		end
+		frame.btn = dbtn
 
 		-- Create dropdown list
 		local ddlist =  CreateFrame("Frame",nil,frame, "BackdropTemplate")
@@ -3548,11 +3550,6 @@
 		local ddlistchk = CreateFrame("FRAME", nil, ddlist)
 		ddlistchk:SetHeight(16); ddlistchk:SetWidth(16)
 		ddlistchk.t = ddlistchk:CreateTexture(nil, "ARTWORK"); ddlistchk.t:SetAllPoints(); ddlistchk.t:SetTexture("Interface\\Common\\UI-DropDownRadioChecks"); ddlistchk.t:SetTexCoord(0, 0.5, 0.5, 1.0);
-
-		-- Lock dropdown menu if there is only one list item and it is 3 hyphens
-		if #items == 1 and items[1] == "---" then
-			dbtn:Disable()
-		end
 
 		-- Create dropdown list items
 		for k, v in pairs(items) do
